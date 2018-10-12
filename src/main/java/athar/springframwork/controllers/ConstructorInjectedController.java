8f679d4d1d271186fc1ab82c315fcd1abdf6a36b
Hafiz-ATHAR <h.m.athar1@hotmail.com>
2018-10-12 14:41:41 -0700

    Add service layer

---
+++ src/main/java/athar/springframwork/controllers/ConstructorInjectedController.java
@@ -0,0 +1,23 @@
+package athar.springframwork.controllers;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+import athar.springframwork.services.Movie;
+
+@Controller
+public class ConstructorInjectedController {
+
+	private Movie movie;
+	
+	public ConstructorInjectedController(
+		@Qualifier("constructorMovieService")	Movie movie) {
+		
+		this.movie = movie;
+	}
+	
+	public String getMovie() {
+		
+		return movie.fetchMovie();
+	}
+}
---
+++ src/main/java/athar/springframwork/services/MovieServiceImpl.java
@@ -0,0 +1,14 @@
+package athar.springframwork.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieServiceImpl implements Movie {
+
+	public static final String MOVIE = "Interstaller";
+
+    @Override
+    public String fetchMovie() {
+        return MOVIE;
+    }
+}
