553d20cedb67037a3ef191ad02b108fe0c6d2907
Hafiz-ATHAR <h.m.athar1@hotmail.com>
2018-10-12 16:00:58 -0700

    Revert "cd \"
    This reverts commit 609b91f5f247e2f3352e195d8bce3f1c21174239.

---
+++ src/main/java/athar/springframwork/controllers/PropertyInjectedController.java
@@ -0,0 +1,17 @@
+package athar.springframwork.controllers;
+
+import org.springframework.stereotype.Controller;
+
+import athar.springframwork.services.MovieServiceImpl;
+
+@Controller
+public class PropertyInjectedController {
+
+	private MovieServiceImpl movie;
+	
+public String getMovie() {
+		
+		return movie.fetchMovie();
+	}
+	
+}
---
+++ src/main/java/athar/springframwork/services/ConstructorMovieService.java
@@ -0,0 +1,15 @@
+package athar.springframwork.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConstructorMovieService implements Movie{
+
+	@Override
+	public String fetchMovie() {
+		return "A walk to remember- From Constructor Movie Service ";
+	}
+	
+	
+
+}
