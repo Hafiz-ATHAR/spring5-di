9896f2a8486bd8df40302287ced9daef4ffd3d1d
Hafiz-ATHAR <h.m.athar1@hotmail.com>
2018-10-13 05:21:55 -0700

    Add Primary Movie Service

---
+++ src/main/java/athar/springframwork/services/PrimaryMovieService.java
@@ -0,0 +1,16 @@
+package athar.springframwork.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+public class PrimaryMovieService implements Movie{
+
+	@Override
+	public String fetchMovie() {
+		// TODO Auto-generated method stub
+		return "The Departed- From Primary Movie Service";
+	}
+
+}
